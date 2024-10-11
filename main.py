d7676fe82a95f8a86640ce58ecb82a383ea7f74e
lxrs312 <larslehmann01@web.de>
2024-10-11 09:13:14 +0200

    new guessing competition commands

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class VERIFY(discord.ui.View):
 intents = discord.Intents.default()
 client = commands.Bot(command_prefix=style.command_prefix_text, intents=intents)
 
-def write_json(data: dict) -> str:
+def write_json(data: dict, file_path) -> str:
     """writing to a json-file
 
     Args:
@@ -46,20 +46,20 @@ def write_json(data: dict) -> str:
         str: Exception as str (optional)
     """
     try:
-        with open(FILE_PATH, "w", encoding="utf8") as f:
+        with open(file_path, "w", encoding="utf8") as f:
             json.dump(data, f, indent=4)
     except Exception as e:
         return e
 
-def load_json() -> tuple[dict, str]:
+def load_json(file_path) -> tuple[dict, str]:
     """loading a json file into a dict
 
     Returns:
         tuple[dict, str]: dictionary, Exception as str
     """
-    if os.path.exists(FILE_PATH):
+    if os.path.exists(file_path):
         try:
-            with open(FILE_PATH, 'r', encoding="utf8") as file:
+            with open(file_path, 'r', encoding="utf8") as file:
                 return json.load(file), None
         except json.JSONDecodeError as e:
             return {}, e
@@ -116,14 +116,53 @@ def get_embed(star_string: str, message: str, product: str, new_vouch_nr: int, u
 
 def get_verify_embed():
     now = datetime.now()
-    embed = discord.Embed(title="Back up this Server via clicking on the Button below!",
-                      description="This will allow us to pull you into a new server, whenever anything happens to this one.",
+    embed = discord.Embed(title=style.verify_embed_title,
+                      description=style.verify_embed_description,
                       colour=style.color, timestamp=now)
 
     embed.set_author(name=client.user.name)
     embed.set_image(url=env_vars['verify_backup_image'])
     embed.set_footer(text=client.user.name, icon_url=env_vars['icon_url'])
     return embed
+
+def get_guess_embed(start: int, end: int, price):
+    now = datetime.now()
+    embed = discord.Embed(title=style.guess_embed_title,
+                      description=style.guess_embed_description,
+                      colour=style.color, timestamp=now)
+
+    embed.set_author(name=client.user.name)
+    embed.add_field(name="Numbers",
+                value=f"Guess between **{start}** and **{end}**!",
+                inline=True)
+    embed.add_field(name="Price",
+                value=f"**{price}**",
+                inline=True)
+    embed.set_footer(text=client.user.name, icon_url=env_vars['icon_url'])
+    return embed
+
+def get_winner_embed(user, price, actual_number):
+    now = datetime.now()
+    embed = discord.Embed(title=style.guess_winner_embed_title,
+                      description=style.guess_winner_embed_description.format(user, price, actual_number),
+                      colour=style.color, timestamp=now)
+
+    embed.set_author(name=client.user.name)
+    embed.set_footer(text=client.user.name, icon_url=env_vars['icon_url'])#
+    return embed
+
+async def handle_guess_end(ctx: discord.Interaction) -> None:
+    """Handles the logic for ending the guessing competition."""
+    try:
+        os.remove(style.guess_path)
+    except Exception as exc:
+        await ctx.followup.send(style.guess_error_text.format(exc), ephemeral=True)
+        return
+    
+    if not ctx.user.guild_permissions.administrator:
+        return
+    
+    await ctx.followup.send(style.guess_end_text, ephemeral=True)
 
 @client.event
 async def on_ready() -> None:
@@ -160,7 +199,7 @@ def register_commands() -> None:
             return
 
         # load data
-        data, load_error = load_json()
+        data, load_error = load_json(FILE_PATH)
         if load_error:
             logging.error(f"Error while loading {env_vars['path_to_json']}: {load_error}")
             await ctx.followup.send(style.json_error_text, ephemeral=True)
@@ -193,7 +232,7 @@ def register_commands() -> None:
         }
 
         # write data
-        write_error = write_json(data)
+        write_error = write_json(data, FILE_PATH)
         if write_error:
             logging.error(f"Error while saving {env_vars['path_to_json']}: {write_error}")
             await ctx.followup.send(style.json_error_text, ephemeral=True)
@@ -209,11 +248,12 @@ def register_commands() -> None:
         
         logging.info(f"Successfully saved vouch {new_vouch_nr}.")
     
-    @client.tree.command(name="verify_embed", description="Sends a verification embed with a button", guild=discord.Object(id=env_vars['guild_id']))
+    @client.tree.command(name=style.command_verify_name_text, description=style.command_verify_description_text, guild=discord.Object(id=env_vars['guild_id']))
     async def verify_embed(ctx: discord.Interaction) -> None:
 
         if not ctx.user.guild_permissions.administrator:
-            await ctx.response.send_message("You do not have permission to use this command.", ephemeral=True)
+            await ctx.response.send_message(style.admin_error_text, ephemeral=True)
+            return
 
         # Defer the response (shows bot is thinking)
         await ctx.response.defer()
@@ -223,6 +263,92 @@ def register_commands() -> None:
 
         # Send the embed along with the view (which contains the button)
         await ctx.followup.send(embed=embed, view=VERIFY(env_vars['verify_url']))
+
+    @client.tree.command(name=style.command_guess_create_name_text, description=style.command_guess_create_description_text, guild=discord.Object(id=env_vars['guild_id']))
+    async def guess_create(ctx: discord.Interaction, start: int, end: int, actual_number: int, price: str):
+        if not ctx.user.guild_permissions.administrator:
+            await ctx.response.send_message(style.admin_error_text, ephemeral=True)
+            return
+
+        # Defer the response (shows bot is thinking)
+        await ctx.response.defer()
+        
+        # If a guess is currently active, end it before
+        if os.path.isfile(style.guess_path):
+            await ctx.followup.send(style.guess_create_still_active_text, ephemeral=True)
+            return
+        
+        data = {
+            "winner_number": actual_number,
+            "start": start,
+            "end": end,
+            "price": price,
+            "participants": 0
+        }
+        
+        # Write current info to guess.json
+        write_json(data, style.guess_path)
+        
+        embed = get_guess_embed(start, end, price)
+
+        await ctx.followup.send(embed=embed)
+
+    @client.tree.command(name=style.command_guess_info_name_text, description=style.command_guess_info_description_text, guild=discord.Object(id=env_vars['guild_id']))
+    async def guess_info(ctx: discord.Interaction):
+        if not ctx.user.guild_permissions.administrator:
+            await ctx.response.send_message(style.admin_error_text, ephemeral=True)
+            return
+
+        # Defer the response (shows bot is thinking)
+        await ctx.response.defer(ephemeral=True)
+        
+        data, load_error = load_json(style.guess_path)
+        if load_error or data == {}:
+            await ctx.followup.send(style.guess_error_text.format(load_error), ephemeral=True)
+            return
+        
+        await ctx.followup.send(str(data), ephemeral=True)
+
+    @client.tree.command(name=style.command_guess_end_name_text, description=style.command_guess_end_description_text, guild=discord.Object(id=env_vars['guild_id']))
+    async def guess_end(ctx: discord.Interaction):
+        if not ctx.user.guild_permissions.administrator:
+            await ctx.response.send_message(style.admin_error_text, ephemeral=True)
+            return
+
+        await ctx.response.defer(ephemeral=True)
+        await handle_guess_end(ctx)
+
+    @client.tree.command(name=style.command_guess_name_text, description=style.command_guess_description_text, guild=discord.Object(id=env_vars['guild_id']))
+    async def guess(ctx: discord.Interaction, number: int):
+        await ctx.response.defer(ephemeral=True)
+        
+        if ctx.channel.id == env_vars['channel_id']:
+            await ctx.followup.send(style.guess_wrong_channel_text)
+            return
+        
+        if not os.path.isfile(style.guess_path):
+            await ctx.followup.send(style.guess_no_active_competition, ephemeral=True)
+            return
+        
+        data, load_error = load_json(style.guess_path)
+        if load_error:
+            await ctx.followup.send(style.guess_error_text.format(""), ephemeral=True)
+            return
+        
+        data['participants'] += 1
+        winner_number = data.get('winner_number')
+        price = data.get('price')
+        
+        if number == winner_number:
+            embed = get_winner_embed(ctx.user.mention, price, winner_number)
+            await handle_guess_end(ctx)
+            await ctx.followup.send(embed=embed)
+            return
+        
+        write_json(data, style.guess_path)
+        await ctx.followup.send(style.guess_better_luck_text)
+        
+        
 
 def run() -> None:
     """called to start the bot
--- style.py
+++ style.py
@@ -6,6 +6,19 @@ vouch_nr_text = "Vouch Nr."
 vouch_by_text = "Vouched By:"
 vouch_product = "Product purchased:"
 
+admin_error_text = "You do not have permission to use this command."
+guess_error_text = "An error happened during loading. Maybe there is no active guessing-competition? {}"
+guess_wrong_channel_text = "This is definetly the wrong channel for what you are trying to do!"
+
+verify_embed_title = "Back up this Server via clicking on the Button below!"
+verify_embed_description = "This will allow us to pull you into a new server, whenever anything happens to this one."
+
+guess_embed_title = "A new guessing competition has started!"
+guess_embed_description = "Please use the **/guess**-command to participate in the newest **guess**-competition!"
+
+guess_winner_embed_title = "Congratulations!"
+guess_winner_embed_description = "{} has won the current competition. You will be getting {}. **The correct number was {}.**"
+
 vouch_channel_name = "🧊︱vouches-{}"
 
 command_name_text = "vouch"
@@ -13,6 +26,22 @@ command_description_text = "Please leave a review for the services provided!"
 
 command_verify_name_text = "verify_embed"
 command_verify_description_text = "Returns the verify embed."
+
+command_guess_create_name_text = "guess_create"
+command_guess_create_description_text = "Creates a guess competition."
+guess_create_still_active_text = "A guessing competition is still active. Please use /guess_end to end it before creating a new one!"
+
+command_guess_info_name_text = "guess_info"
+command_guess_info_description_text = "Returns info about the current guessing competition."
+
+command_guess_end_name_text = "guess_end"
+command_guess_end_description_text = "Ends the current guessing competition."
+guess_end_text = "The guess competition has been terminated."
+
+command_guess_name_text = "guess"
+command_guess_description_text = "Guess the current number!"
+guess_no_active_competition = "There is no active guess competition!"
+guess_better_luck_text = "Unfortunately that wasn't the correct number. Please try again later."
 
 command_stars_description_text = "Amount of stars you would evaluate the service with."
 command_message_description_text = "Please leave a message."
@@ -27,4 +56,6 @@ wrong_channel_error_text = "Please refrain from using the Bot outside of the Vou
 date_format = "%d/%m/%Y, %H:%M:%S"
 
 logging_format = '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
+
+guess_path = "guess.json"
 
